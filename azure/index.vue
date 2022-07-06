39ae64761f74d8f817d60912a580b749aebbbb70
aguywhosaguy <caseypeg8@gmail.com>
2022-07-06 15:21:12 -0500

    yea

--- azure/index.vue
+++ azure/index.vue
@@ -1,9 +1,37 @@
 <script>
-    
+    export default {
+        data() {
+            return {
+                ores: [
+                    {name: 'coal', av: 275000, cv: 1}
+                ]
+            }
+        }
+    }
 </script>
 
+<template>
+    <form>
+        <li v-for="ore in ores" class="grid">
+            <label>{{ore.name}}</label>
+            <label>{{ore.av}}</label>
+            <label>{{ore.cv}}</label>
+        </li>
+    </form>
 
+</template>
 
 <style lang="scss">
-
+    .tile {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-rows: repeat(49, 1fr);
+        grid-gap: 1px;
+        background-color: #eee;
+    }
 </style>
